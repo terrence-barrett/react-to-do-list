b49b7e566804c675b6829f38a3a8971e8506b67d
Rice Warlord <barrettbarrettbarrett@protonmail.com>
2023-11-15 11:29:56 -0500

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+    import './App.css';
+    import { useState } from 'react';
+    import Task from './Task';
+//------------------------------------------------------------------
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    function App() {
+        const [todoList, setTodoList] = useState([]);
+        const [newTask, setNewTask] = useState("")
 
-export default App;
+
+        const handleChange = (event) => {
+            setNewTask(event.target.value)
+        }
+
+        const addTask =()=>{
+
+            const task = {
+            id: todoList.length === 0? 1: todoList[todoList.length-1].id + 1,
+            taskName: newTask,
+            completed: false,
+            }
+            setTodoList([...todoList, task])
+        }
+
+        const deleteTask =(id)=>{
+            setTodoList(todoList.filter((task)=>task.id !==id));
+        }
+        
+        const completeTask =(id)=>{
+            setTodoList(
+            todoList.map((task)=>{
+                if(task.id === id){
+                    return{...task, completed: true}
+                } else{
+                    return task;
+                }
+            })
+            )
+        }
+
+        return(
+            <div className='App'>
+                
+                <div className='addTask'>
+                    <p>TODO LIST</p>
+                    <input placeholder='Write your to-do list...' onChange={handleChange}/>
+                    <button id="b-add" onClick={addTask}>Add Task</button>
+                </div>
+
+                <p id='list-p'>List of tasks</p>
+                <div className='list'>
+                    {todoList.map((task)=>{
+                        return(
+                            <Task taskName={task.taskName}
+                            id={task.id}
+                            completed={task.completed}
+                            deleteTask={deleteTask}
+                            completeTask={completeTask}/>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+    export default App;
---
+++ src/Task.js
@@ -0,0 +1,16 @@
+
+
+const Task = (props)=>{
+    return(
+        <div className="task"
+        style={{ backgroundColor: props.completed? 'green':'red' }}>
+            <h1>{props.taskName}</h1>
+            <div className="buttons">
+                <button onClick={()=>props.completeTask(props.id)}>Complete</button>
+                <button onClick={()=>props.deleteTask(props.id)}>X</button>
+            </div>
+        </div>
+    )
+}
+
+export default Task;
